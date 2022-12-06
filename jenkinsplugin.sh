31946f1aab1bb8510445181b7d8f5b213a8e28c7
11271817 <38833021+11271817@users.noreply.github.com>
2022-12-06 19:17:56 +0530

    added the jenkins-cli path

--- jenkinsplugin.sh
+++ jenkinsplugin.sh
@@ -1,6 +1,7 @@
+curl '54.161.224.184:8080/jnlpJars/jenkins-cli.jar' > jenkins-cli.jar
 UPDATE_LIST=$( java -jar jenkins-cli.jar -s http://54.161.224.184:8080/ list-plugins | grep -e '$' | awk '{ print $1 }' ); 
 if [ ! -z "${UPDATE_LIST}" ]; then 
     echo Updating Jenkins Plugins: ${UPDATE_LIST}; 
     java -jar jenkins-cli.jar -s http://54.161.224.184:8080/ install-plugin ${UPDATE_LIST};
-    java -jar jenkins-cli.jar -s http://3.80.59.102:8080/ safe-restart;
+    java -jar jenkins-cli.jar -s http://54.161.224.184:8080/ safe-restart;
 fi
